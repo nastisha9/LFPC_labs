5496e3642b0a0aee1b24c3783a475ea318efeece
nastisha9 <91118783+nastisha9@users.noreply.github.com>
2022-02-05 01:27:42 +0200

    modify some parts

--- Lab1/Main.java
+++ Lab1/Main.java
@@ -1,18 +1,23 @@
 package Lab1;
 import java.util.*;
+import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(System.in);
         String str;
-        HashMap<Character, HashMap<Character, Character>> fa = new HashMap<>();
+        HashMap<Character, HashMap<Character, Character>> FA = new HashMap<>();
 
-        System.out.println("Print the derivation rules in column (e.g S -> aB). Enter 'stop' to finish. ");
+        // reading the input from a file
+        String path = "C:/users/Nastea/LFPC/src/Lab1/var11.txt";
+        File file = new File(path);
+        Scanner scan = new Scanner(file);
 
-        // reading the input until stop
+        // reading the input until stop word
         while(true) {
-            str = scanner.nextLine();
+            str = scan.nextLine();
 
             if (str.equals("stop"))
                 break;
@@ -20,13 +25,13 @@ public class Main {
             // splitting the string into 2 parts
             String[] parts = str.split(" -> ");
 
-            if(fa.containsKey(parts[0].charAt(0))) {
+            // check if the map contains a mapping for the specified key
+            if(FA.containsKey(parts[0].charAt(0))) {
                 if(parts[1].length() == 2) {
-                    fa.get(parts[0].charAt(0)).put(parts[1].charAt(0), parts[1].charAt(1));
+                    FA.get(parts[0].charAt(0)).put(parts[1].charAt(0), parts[1].charAt(1));
                 } else {
-                    fa.get(parts[0].charAt(0)).put(parts[1].charAt(0), '$');
+                    FA.get(parts[0].charAt(0)).put(parts[1].charAt(0), '$');
                 }
-
             } else {
                 HashMap<Character, Character> sr = new HashMap<>();
                 if(parts[1].length() == 2) {
@@ -34,26 +39,29 @@ public class Main {
                 } else {
                     sr.put(parts[1].charAt(0), '$');
                 }
-                fa.put(parts[0].charAt(0), sr);
+                FA.put(parts[0].charAt(0), sr);
             }
         }
 
-        System.out.println("Print the string to be verified: ");
+        System.out.println("\n Print the string to be verified: ");
         str = scanner.nextLine();
         char next = 'S';
+        StringBuilder route = new StringBuilder(); // for showing the route
+        route.append("S"); // start character
 
         for(int i = 0; i < str.length(); i++) {
-            if(fa.get(next).containsKey(str.charAt(i))) {
-                next = fa.get(next).get(str.charAt(i));
+            if(FA.get(next).containsKey(str.charAt(i))) {
+                next = FA.get(next).get(str.charAt(i));
+                if (next != '$')
+                    route.append(" -> " + next);
             }
 
             if(i == str.length() - 1) {
-                if(next == '$')
-                {
+                if(next == '$') {
                     System.out.println("The string '" + str + "' is accepted");
+                    System.out.println("The route to obtain the string is: " + route.toString());
                 }
-                else
-                    System.out.println("The string '" + str + "' is rejected");
+                else System.out.println("The string '" + str + "' is rejected");
             }
         }
     }
